7b6f31726167b6e2e977e57284c8cd628ea2af4b
ashfaaq <muzammil.ashfaaq@gmail.com>
2025-02-16 13:02:22 +0530

    second commit

--- src/main/java/clculator.java
+++ src/main/java/clculator.java
@@ -5,9 +5,17 @@ public class clculator {
     public int diff(int a, int b){
         return a-b;
     }
+    public int mult(int a, int b){
+        return a*b;
+    }
+    public int div(int a, int b){
+        return a/b;
+    }
     public static void main(String[] args){
         clculator cal = new clculator();
         System.out.println("Sum is = "+cal.add(20,10));
         System.out.println("difference is = "+cal.diff(100,10));
+        System.out.println("Multiply is = "+cal.mult(6,9));
+        System.out.println("Divide is = "+cal.mult(25,5));
     }
 }
