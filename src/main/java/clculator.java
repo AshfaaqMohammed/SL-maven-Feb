fdeea4e47567cfcdb81f2d822a4c15e4ab8cff1c
ashfaaq <muzammil.ashfaaq@gmail.com>
2025-02-17 21:28:19 +0530

    done some changes in cal

--- src/main/java/clculator.java
+++ src/main/java/clculator.java
@@ -14,11 +14,16 @@ public class clculator {
     public void show(){
         System.out.println("Hello Jenkins");
     }
+    public void greeting(){
+        System.out.println("this is from greetings");
+    }
     public static void main(String[] args){
         clculator cal = new clculator();
         System.out.println("Sum is = "+cal.add(20,10));
         System.out.println("difference is = "+cal.diff(100,10));
         System.out.println("Multiply is = "+cal.mult(6,9));
         System.out.println("Divide is = "+cal.div(25,5));
+        cal.show();
+        cal.greeting();
     }
 }
